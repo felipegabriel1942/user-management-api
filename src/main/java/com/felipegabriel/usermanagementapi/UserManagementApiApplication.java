7a57d85ceef0e88ec4e1afc792a18e66a2f9bd46
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2021-08-14 23:46:02 -0300

    first commit

---
+++ src/main/java/com/felipegabriel/usermanagementapi/UserManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.felipegabriel.usermanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipegabriel/usermanagementapi/UserManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipegabriel.usermanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
